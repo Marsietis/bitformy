397e6a85ecacd07832e19d6a876c06e7d42f04b6
Marsietis <c3xrk8o3@addy.io>
2025-06-12 18:12:48 +0300

    Add new form creation page and update sidebar navigation

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ Route::get('meetings/{id}', [FormController::class, 'show'])->name('meeting.show
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', action: fn() => Inertia::render('Dashboard'))
         ->name('dashboard');
+    Route::get('form/new', action: fn() => Inertia::render('form/CreateForm'));
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
