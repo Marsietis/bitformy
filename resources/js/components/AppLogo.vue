b9a0c8821558cb4a4c00752ffeceabfc2e69f66d
marsietis <c3xrk8o3@addy.io>
2025-11-01 01:19:17 +0200

    Remove unused AppLogoIcon import and clean up sidebar imports

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
-</script>
-
 <template>
     <div class="grid flex-1 text-left text-sm">
         <span class="font-bold text-black dark:text-white text-5xl">Bitformy</span>
